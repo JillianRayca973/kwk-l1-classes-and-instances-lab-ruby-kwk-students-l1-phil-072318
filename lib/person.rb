917b682b067a2005faafe6b5a05e183a24313d5d
Jill  Rayca <rkrayca@yahoo.com>
2018-07-26 18:33:08 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
 # person.rb
 
 class Person
+  
+  def initialize(name)
+    @name = name
+  end
+  
+end
+
+adele_goldberg = Person.new("Adele Goldberg")
+snoopy = Dog.new("Snoopy")
+lassie = Dog.new("Lassie")
